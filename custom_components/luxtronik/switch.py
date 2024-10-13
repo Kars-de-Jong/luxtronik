3e4c0f7b94e9feb94bdf25f6f3ab1f6b8f8a1b6e
Kars de Jong <jongk@linux-m68k.org>
2024-10-13 14:30:59 +0200

    Fix switch.py issues
     - Prepare for 2025.1.
     - Don't inherit from LuxtronikBinarySensor to fix warning about entity_category.

--- custom_components/luxtronik/switch.py
+++ custom_components/luxtronik/switch.py
@@ -2,18 +2,17 @@
 # region Imports
 from typing import Any
 
-from homeassistant.components.binary_sensor import (DEVICE_CLASS_HEAT,
-                                                    DEVICE_CLASS_RUNNING)
+from homeassistant.components.binary_sensor import BinarySensorDeviceClass
+from homeassistant.components.sensor import ENTITY_ID_FORMAT
 from homeassistant.components.switch import SwitchEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity import EntityCategory
+from homeassistant.helpers.entity import DeviceInfo, EntityCategory
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.restore_state import RestoreEntity
 
 from . import LuxtronikDevice
-from .binary_sensor import LuxtronikBinarySensor
-from .const import (CONF_LANGUAGE_SENSOR_NAMES, DOMAIN, LOGGER,
+from .const import (ATTR_EXTRA_STATE_ATTRIBUTE_LUXTRONIK_KEY, DOMAIN, LOGGER,
                     LUX_SENSOR_EFFICIENCY_PUMP, LUX_SENSOR_HEATING_THRESHOLD,
                     LUX_SENSOR_MODE_COOLING, LUX_SENSOR_MODE_DOMESTIC_WATER,
                     LUX_SENSOR_MODE_HEATING, LUX_SENSOR_PUMP_OPTIMIZATION,
@@ -52,20 +51,20 @@ async def async_setup_entry(
     text_pump_heat_control = get_sensor_text(lang, 'pump_heat_control')
     entities += [
         LuxtronikSwitch(
-            hass=hass, luxtronik=luxtronik, deviceInfo=device_info,
+            hass=hass, luxtronik=luxtronik, device_info=device_info,
             sensor_key=LUX_SENSOR_REMOTE_MAINTENANCE, unique_id='remote_maintenance',
             name=f"{text_remote_maintenance}", icon='mdi:remote-desktop',
-            device_class=DEVICE_CLASS_HEAT, entity_category=EntityCategory.CONFIG),
+            device_class=BinarySensorDeviceClass.HEAT, entity_category=EntityCategory.CONFIG),
         LuxtronikSwitch(
-            hass=hass, luxtronik=luxtronik, deviceInfo=device_info,
+            hass=hass, luxtronik=luxtronik, device_info=device_info,
             sensor_key=LUX_SENSOR_EFFICIENCY_PUMP, unique_id='efficiency_pump',
             name=f"{text_efficiency_pump}", icon='mdi:leaf-circle',
-            device_class=DEVICE_CLASS_HEAT, entity_category=EntityCategory.CONFIG),
+            device_class=BinarySensorDeviceClass.HEAT, entity_category=EntityCategory.CONFIG),
         LuxtronikSwitch(
-            hass=hass, luxtronik=luxtronik, deviceInfo=device_info,
+            hass=hass, luxtronik=luxtronik, device_info=device_info,
             sensor_key='parameters.ID_Einst_P155_PumpHeatCtrl', unique_id='pump_heat_control',
             name=text_pump_heat_control, icon='mdi:pump',
-            device_class=DEVICE_CLASS_HEAT, entity_category=EntityCategory.CONFIG,
+            device_class=BinarySensorDeviceClass.HEAT, entity_category=EntityCategory.CONFIG,
             entity_registry_enabled_default=False),
     ]
 
@@ -75,21 +74,21 @@ async def async_setup_entry(
         text_heating_threshold = get_sensor_text(lang, 'heating_threshold')
         entities += [
             LuxtronikSwitch(
-                hass=hass, luxtronik=luxtronik, deviceInfo=deviceInfoHeating,
+                hass=hass, luxtronik=luxtronik, device_info=deviceInfoHeating,
                 sensor_key=LUX_SENSOR_PUMP_OPTIMIZATION, unique_id='pump_optimization',
                 name=text_pump_optimization, icon='mdi:tune',
-                device_class=DEVICE_CLASS_HEAT, entity_category=EntityCategory.CONFIG),
+                device_class=BinarySensorDeviceClass.HEAT, entity_category=EntityCategory.CONFIG),
             LuxtronikSwitch(
                 on_state=LuxMode.automatic.value, off_state=LuxMode.off.value,
-                hass=hass, luxtronik=luxtronik, deviceInfo=deviceInfoHeating,
+                hass=hass, luxtronik=luxtronik, device_info=deviceInfoHeating,
                 sensor_key=LUX_SENSOR_MODE_HEATING, unique_id='heating',
                 name=text_heating_mode, icon='mdi:radiator', icon_off='mdi:radiator-off',
-                device_class=DEVICE_CLASS_HEAT),
+                device_class=BinarySensorDeviceClass.HEAT),
             LuxtronikSwitch(
-                hass=hass, luxtronik=luxtronik, deviceInfo=deviceInfoHeating,
+                hass=hass, luxtronik=luxtronik, device_info=deviceInfoHeating,
                 sensor_key=LUX_SENSOR_HEATING_THRESHOLD, unique_id='heating_threshold',
                 name=f"{text_heating_threshold}", icon='mdi:download-outline',
-                device_class=DEVICE_CLASS_HEAT, entity_category=EntityCategory.CONFIG)
+                device_class=BinarySensorDeviceClass.HEAT, entity_category=EntityCategory.CONFIG)
         ]
 
     deviceInfoDomesticWater = hass.data[f"{DOMAIN}_DeviceInfo_Domestic_Water"]
@@ -100,13 +99,13 @@ async def async_setup_entry(
             LuxtronikSwitch(
                 on_state=LuxMode.automatic.value, off_state=LuxMode.off.value,
                 hass=hass, luxtronik=luxtronik,
-                deviceInfo=deviceInfoDomesticWater,
+                device_info=deviceInfoDomesticWater,
                 sensor_key=LUX_SENSOR_MODE_DOMESTIC_WATER,
                 unique_id='domestic_water',
                 name=text_domestic_water_mode_auto, icon='mdi:water-boiler-auto', icon_off='mdi:water-boiler-off',
-                device_class=DEVICE_CLASS_HEAT),
+                device_class=BinarySensorDeviceClass.HEAT),
         ]
-        
+
     deviceInfoCooling = hass.data[f"{DOMAIN}_DeviceInfo_Cooling"]
     if deviceInfoCooling is not None:
         text_cooling_mode_auto = get_sensor_text(
@@ -115,22 +114,30 @@ async def async_setup_entry(
             LuxtronikSwitch(
                 on_state=LuxMode.automatic.value, off_state=LuxMode.off.value,
                 hass=hass, luxtronik=luxtronik,
-                deviceInfo=deviceInfoCooling,
+                device_info=deviceInfoCooling,
                 sensor_key=LUX_SENSOR_MODE_COOLING,
                 unique_id='cooling',
                 name=text_cooling_mode_auto, icon='mdi:snowflake',
-                device_class=DEVICE_CLASS_HEAT)
-        ]        
+                device_class=BinarySensorDeviceClass.HEAT)
+        ]
 
     async_add_entities(entities)
 # endregion Setup
 
 
-class LuxtronikSwitch(LuxtronikBinarySensor, SwitchEntity, RestoreEntity):
+class LuxtronikSwitch(SwitchEntity, RestoreEntity):
     """Representation of a Luxtronik switch."""
 
     def __init__(
         self,
+        luxtronik: LuxtronikDevice,
+        device_info: DeviceInfo,
+        sensor_key: str,
+        unique_id: str,
+        name: str,
+        icon: str,
+        device_class: str,
+        icon_off: str = None,
         on_state: str = True,
         off_state: str = False,
         *args: Any,
@@ -138,8 +145,18 @@ class LuxtronikSwitch(LuxtronikBinarySensor, SwitchEntity, RestoreEntity):
     ) -> None:
         """Initialize a new Luxtronik switch."""
         super().__init__(**kwargs)
+        self._luxtronik = luxtronik
+        self._sensor_key = sensor_key
+        self.entity_id = ENTITY_ID_FORMAT.format(f"{DOMAIN}_{unique_id}")
+        self._attr_unique_id = self.entity_id
+        self._attr_device_info = device_info
+        self._attr_name = name
+        self._attr_icon = icon
+        self._icon_off = icon_off
+        self._attr_device_class = device_class
         self._on_state = on_state
         self._off_state = off_state
+        self._attr_extra_state_attributes = {ATTR_EXTRA_STATE_ATTRIBUTE_LUXTRONIK_KEY: sensor_key}
 
     async def async_turn_on(self, **kwargs: Any) -> None:
         """Turn the switch on."""
@@ -154,3 +171,22 @@ class LuxtronikSwitch(LuxtronikBinarySensor, SwitchEntity, RestoreEntity):
             '.')[1], self._off_state, use_debounce=False,
             update_immediately_after_write=True)
         self.schedule_update_ha_state(force_refresh=True)
+
+    @property
+    def is_on(self):
+        """Return true if binary sensor is on."""
+        value = self._luxtronik.get_value(self._sensor_key) == self._on_state
+        return value
+
+    @property
+    def icon(self):  # -> str | None:
+        """Return the icon to be used for this entity."""
+        if not self.is_on and self._icon_off is not None:
+            return self._icon_off
+        if self._attr_icon is None:
+            super().icon
+        return self._attr_icon
+
+    def update(self):
+        """Get the latest status and use it to update our sensor state."""
+        self._luxtronik.update()
